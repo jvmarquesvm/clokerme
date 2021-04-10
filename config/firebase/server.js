5ad643cc9c0224876eed675f88b6f0cc8f5f89bc
Joao Victor <joaovictor2@hotmail.com>
2021-04-10 01:27:11 -0300

    #9 - Configurando variavel de ambiente firebase

--- config/firebase/server.js
+++ config/firebase/server.js
@@ -12,7 +12,7 @@ const app = firebaseServer.apps.length
           
             project_id: process.env.PROJECT_ID,
             private_key_id: process.env.PRIVATE_KEY_ID,
-            private_key: process.env.PRIVATE_KEY,
+            private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),            
             client_email: process.env.CLIENT_EMAIL,
             client_id: process.env.CLIENT_ID,
             client_cert: process.env.CLIENT_CERT
